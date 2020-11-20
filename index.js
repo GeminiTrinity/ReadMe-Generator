13a75a2e6083574f32c63ee18b24827e5613153d
Jacob Krueger <jmk4u14@gmail.com>
2020-11-19 23:34:29 -0500

    Added questions to index file

--- index.js
+++ index.js
@@ -1,9 +1,75 @@
 // array of questions for user
 const inquirer = require("inquirer");
 
-const questions = [
-
-];
+inquirer
+    .prompt([
+        {
+            name: "title",
+            type: "input",
+            message: "What would you like the title to be?"
+        },
+        {
+            name: "description",
+            type: "input",
+            message: "Write a description of what your project does."
+        },
+        {
+            name: "link",
+            type: "input",
+            message: "Include the live link to the deployed app:"
+        },
+        {
+            name: "installation",
+            type: "input",
+            message: "Write the steps required to install your project."
+        },
+        {
+            name: "usage",
+            type: "input",
+            message: "Provide instructions on how to use your project."
+        },
+        {
+            name: "collaborators",
+            type: "input",
+            message: "Include any collaborators, as well as their GitHubs and emails, here (skip if none):"
+        },
+        {
+            name: "third-party",
+            type: "input",
+            message: "Include any third-parties that require attribution (skip if none):"
+        },
+        {
+            name: "tutorials",
+            type: "input",
+            message: "Include any tutorial links here (skip if none):"
+        },
+        {
+            name: "license",
+            type: "list",
+            message: "Please choose the license for this project.",
+            choices: ["MIT", "ISC", "Apache-2.0", "N/A"]
+        },
+        {
+            name: "github",
+            type: "input",
+            message: "What is your GitHub username?"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "What is your email address?"
+        },
+        {
+            name: "phone",
+            type: "input",
+            message: "What is your phone number? (xxx-xxx-xxxx)"
+        },
+    ])
+// const questions = [
+//     name:
+//     type:
+//     message:
+// ];
 
 // function to write README file
 function writeToFile(fileName, data) {
